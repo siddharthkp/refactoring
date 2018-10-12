cc38940c6fdf323d6f396d33d23f901abb5ed6a0
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-10-12 17:09:12 +0530

    drill label

--- src/drill.js
+++ src/drill.js
@@ -5,6 +5,7 @@ import React from 'react'
 const ProfileForm = props => {
   return (
     <Form>
+      <label>Sign up</label>
       <TextInput placeholder="username" />
       <TextInput type="password" placeholder="password" />
       <TextInput type="password" placeholder="confirm" />
